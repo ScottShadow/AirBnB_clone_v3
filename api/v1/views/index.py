d9acd719763dfa43c801165ac093beeb84dc07c8
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-05-06 13:57:36 +0200

    added api files

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """status route"""
+    return jsonify({"status": "OK"})
