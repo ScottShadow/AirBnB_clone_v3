caf6e27727aa555e1bdd2601d1e77c6382a4c366
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-05-06 14:25:04 +0200

    switching to master branch

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,16 @@ from models import storage
 def status():
     """status route"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """stats route"""
+    all_objs = storage.all()
+    all_objs = len(all_objs)
+    all_cls = []
+    for obj in all_objs:
+        all_cls.append(obj.split('.')[0])
+    all_cls = len(set(all_cls))
+
+    return jsonify({"nb_objects": all_objs, "nb_classes": all_cls})
