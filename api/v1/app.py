845b2dbe913dfe6f28dbf3d72bc734b50c2740ac
ScottShadow <rudaseswascottmc@gmail.com>
2024-05-07 11:35:16 +0200

    added documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 starts a Flask web application
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,14 @@ app.register_blueprint(app_views)
 def teardown(exc):
     """Remove the SQLAlchemy session."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    return JSON formatted 404 status code response
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """import flask views and import storage engine and classes"""
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
